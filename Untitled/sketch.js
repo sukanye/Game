b06f4a4427f4844aef55ffe9ca3436ee6b1031da
sukanye <ray.suk@husky.neu.edu>
2016-02-03 22:11:25 -0500

    need to follow mouse

--- Untitled/sketch.js
+++ Untitled/sketch.js
@@ -1,8 +1,21 @@
 function setup() {
-  
   createCanvas(500,500);
 }
 
 function draw() {
+  background('blue');
+}
+
+function Follow(x,y) {
+  var dotX = x;
+  var dotY = y;
   
+  this.renderDot() = function(){ //what the dot looks like
+    fill('yellow');
+    ellipse(dotX,dotY,50,50);
+  }
+  
+  this.followMouse() = function(){
+    
+  }
 }
